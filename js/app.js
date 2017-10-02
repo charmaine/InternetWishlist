323fa4c46f57d38392b6c61a9342b5724b33abcd
Noah Kettler <nmkettler@gmail.com>
2017-10-02 12:48:39 -0700

    adding scroll to top functionality

--- js/app.js
+++ js/app.js
@@ -50,4 +50,20 @@ $( document ).ready(function() {
   }).mouseleave(function() {
     $canvas.mouseup();
   });
+
+  // Scroll to top of page
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+  
+  //Click event to scroll to top
+  $('.scrollToTop').click(function () {
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+
 });
